cd945d9f664104df32133fe5869a6a22d7d36d5a
CaioFillipe <162356041+CaioFillipe@users.noreply.github.com>
2024-10-25 23:16:05 -0300

    Add files via upload

---
+++ ENTREGASITE.md
@@ -0,0 +1,33 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/XM1GkQ35)
+
+# TÓPICO DO SITE
+- Lana Del Rey
+
+# NOME DO SITE
+- Lana da Web
+
+# URL DO SITE
+
+https://caiofillipe.github.io/TrabalhoPratico-Caio/
+
+# Itens Opcionais Feitos
+
+<h1>FLEXBOX E GRID</h1>
+<img src="provas/grid2.png">
+<img src="provas/grid2.png">
+
+<h1>IFRAME<h1>
+<img src="provas/iframe.png">
+
+<h1>POSICIONAMENTO NÃO ESTÁTICO<h1>
+<img src="provas/position.png">
+
+<h1> INTERAÇÃO COM JAVA SCRIPT / EASTER EGG<h2>
+<img src="provas/js.png">
+
+<h1>ANIMACAO<h1>
+<img src="provas/animacao.png">
+<img src="provas/animacao2.png">
+
+<h1> EASTEREGG <h1>
+<img src="provas/easteregg.png">
--- coisinhas.js
+++ coisinhas.js
@@ -1,12 +1,94 @@
+//Teleporta para o site
 document.getElementById('easteregg').addEventListener('click', function() {
     window.location = 'lanawebsite.html';
 });
 
-const imagem = document.getElementById('imagem-musical2');
-const musica = document.getElementById('cola');
+//Toca musica ao tocar no easteregg
+document.getElementById('imagem-musical2').onclick = function() {
+    const audio1 = document.getElementById('cola');
+    audio1.currentTime = 0; 
+    audio1.play();
+};
 
-imagem.addEventListener('click', function() {
-    musica.play();
-});
+
+document.getElementById('imagem4').onclick = function() {
+    const audio2 = document.getElementById('floridakilos');
+    audio2.currentTime = 0; 
+    audio2.play();
+};
+
+//Toca música característica ao clicar na imagem de cada album
+document.getElementById('borntodieimg').onclick = function() {
+    const audio3 = document.getElementById('borntodie');
+    audio3.currentTime = 0; 
+    audio3.play();
+};
 
 
+document.getElementById('ultraviolenceimg').onclick = function() {
+    const audio4 = document.getElementById('ultraviolence');
+    audio4.currentTime = 0; 
+    audio4.play();
+};
+
+document.getElementById('honeymoonimg').onclick = function() {
+    const audio5 = document.getElementById('honeymoon');
+    audio5.currentTime = 0; 
+    audio5.play();
+};
+
+
+document.getElementById('lustforlifeimg').onclick = function() {
+    const audio6 = document.getElementById('lustforlife');
+    audio6.currentTime = 0; 
+    audio6.play();
+};
+
+document.getElementById('nfrockweelimg').onclick = function() {
+    const audio7 = document.getElementById('nfr');
+    audio7.currentTime = 0; 
+    audio7.play();
+};
+
+
+document.getElementById('chemtrailsimg').onclick = function() {
+    const audio8 = document.getElementById('chemtrails');
+    audio8.currentTime = 0; 
+    audio8.play();
+};
+
+document.getElementById('bluebanistersimg').onclick = function() {
+    const audio9 = document.getElementById('bluebanister');
+    audio9.currentTime = 0; 
+    audio9.play();
+};
+
+
+document.getElementById('blvdimg').onclick = function() {
+    const audio10 = document.getElementById('blvd');
+    audio10.currentTime = 0; 
+    audio10.play();
+};
+
+//Cria botão para voltar ao topo quando usuário rola a tela
+
+window.onscroll = function() {
+    ocultaBotao();
+};
+
+
+function ocultaBotao() {
+    const botao = document.getElementById("voltar-topo");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        botao.style.display = "block";
+    } else {
+        botao.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+}
